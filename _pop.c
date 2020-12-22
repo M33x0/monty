4fa86816c80cf916daa9e22bf38fca7d6fa1386c
m33x0 <michael.mbea@gmail.com>
2020-12-22 17:43:10 +0100

    pop_smoke

---
+++ _pop.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * _pop - removes the top element of the stack
+ *
+ * @head: head of the stack_t
+ * @line_number: line number of the opcode in the file
+ *
+ * Return: void
+ */
+
+void _pop(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	stack_t *tmp;
+
+	current = *head;
+	if (current == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		code_exit = -1;
+		return;
+	}
+	tmp = current->prev;
+	*head = tmp;
+	free(current);
+}

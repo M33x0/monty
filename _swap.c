71f2d690d3eeb40ec6c8b73332ea3c831f16573b
m33x0 <michael.mbea@gmail.com>
2020-12-22 17:43:50 +0100

    swap spit

---
+++ _swap.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+ * _swap - swaps the top two elements of the stack
+ *
+ * @head: head of the stack_t
+ * @line_number: line number of the opcode in the file
+ *
+ * Return: void
+ */
+
+void _swap(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	stack_t *tmp;
+	stack_t *before;
+
+	current = *head;
+	tmp = *head;
+	if (current == NULL || current->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		code_exit = -1;
+		return;
+	}
+	tmp = current;
+	current = current->prev;
+	before = current->prev;
+	tmp->prev = before;
+	tmp->next = current;
+	current->prev = tmp;
+	current->next = NULL;
+	*head = current;
+}

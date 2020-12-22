a9860e118569e1748d88ddf361b4ac967239d928
m33x0 <michael.mbea@gmail.com>
2020-12-22 17:49:35 +0100

    main

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "monty.h"
+
+/**
+ * main - monty
+ * @ac: number of arguments
+ * @av: array of char (arguments)
+ *
+ * Return: 0 (SUCESS)
+ */
+int code_exit = 0;
+
+int main(int ac, char **av)
+{
+
+	FILE *fd_read;
+	char *s;
+	unsigned int line_number = 0;
+	int opcode;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	stack_t *head = NULL;
+
+	if (ac != 2)
+	{
+		s = "USAGE: monty file\n";
+		write(STDERR_FILENO, s, _strlen(s));
+		exit(EXIT_FAILURE);
+
+	}
+	fd_read = fopen(av[1], "r");
+	if (fd_read == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		fclose(fd_read);
+		exit(EXIT_FAILURE);
+	}
+	while (getline(&buffer, &bufsize, fd_read) != -1)
+	{
+		line_number++;
+		opcode = parse(buffer, &head, line_number);
+		if (opcode == -1 || opcode == -2)
+		{
+			free(buffer);
+			fclose(fd_read);
+			free_stack(head);
+			exit(EXIT_FAILURE);
+		}
+	}
+	free(buffer);
+	free_stack(head);
+	fclose(fd_read);
+	return (0);
+}

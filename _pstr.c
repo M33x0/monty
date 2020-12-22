6cc883c3d53ce6c27f7db74351164f1fe76fc3b3
m33x0 <michael.mbea@gmail.com>
2020-12-22 23:42:57 +0100

    fix

---
+++ _pstr.c
@@ -0,0 +1,47 @@
+#include "monty.h"
+
+/**
+ * _pstr - prints the string starting at the top of the stack
+ *
+ * @head: head of a stack_t
+ * @line_number: line nbr of the opcode in the read file
+ *
+ * Return: void
+ */
+
+void _pstr(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	int passed = 0;
+
+	current = *head;
+	if (current == NULL)
+	{
+		fprintf(stderr, "\n");
+		return;
+	}
+	while (current)
+	{
+		if (current->n == 0)
+		{
+			if (passed == 1)
+				printf("\n");
+			return;
+		}
+		if (isascii(current->n) != 0)
+		{
+			printf("%c", current->n);
+			passed = 1;
+		}
+		else
+		{
+			if (passed == 1)
+				printf("\n");
+			return;
+		}
+		current = current->prev;
+	}
+	if (passed == 1)
+		printf("\n");
+	(void)line_number;
+}

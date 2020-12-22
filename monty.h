3bc4a0b82d1d4e55b43c6fd3af9185f3e93dc455
m33x0 <michael.mbea@gmail.com>
2020-12-22 15:44:50 +0100

    prototype

--- monty.h
+++ monty.h
@@ -1,3 +1,12 @@
+#ifndef __MONTY_H
+#define __MONTY_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -9,9 +18,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 /**
  * struct instruction_s - opcode and its function
@@ -23,6 +32,15 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int num_line);
 } instruction_t;
+
+/*Function for opcode*/
+void _push(stack_t **stack, unsigned int num_line);
+void _pall(stack_t **stack, unsigned int num_line);
+void _pint(stack_t **stack, unsigned int num_line);
+void _pop(stack_t **stack, unsigned int num_line);
+void _swap(stack_t **stack, unsigned int num_line);
+
+#endif /*_MONTY_H*/

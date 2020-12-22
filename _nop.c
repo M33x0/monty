1a8f2c122e7659f1cd38ebd24bcf3602ae4dccb9
m33x0 <michael.mbea@gmail.com>
2020-12-22 17:45:31 +0100

    nop+sub

---
+++ _nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * _nop - does not do anything
+ *
+ * @stack: head of the stack_t
+ * @line_number: line nbr of the opcode in the file
+ *
+ * Return: void
+ */
+
+void _nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}

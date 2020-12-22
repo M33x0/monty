1d68b99736176a600b3ed92658c0f1bad297b787
m33x0 <michael.mbea@gmail.com>
2020-12-22 16:17:29 +0100

    added prototype

--- monty.h
+++ monty.h
@@ -1,12 +1,15 @@
 #ifndef __MONTY_H
 #define __MONTY_H
 
-#include <string.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <ctype.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer

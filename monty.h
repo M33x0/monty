57d507b9a5fe3a506687c8a07311de878da62aa6
m33x0 <michael.mbea@gmail.com>
2020-12-22 18:39:17 +0100

    added prototype

--- monty.h
+++ monty.h
@@ -45,6 +45,8 @@ extern int code_exit;
 /*Function for opcode*/
 void _pall(stack_t **stack, unsigned int line_number);
 void _push(stack_t **head, int arg);
+void _div(stack_t **head, unsigned int line_number);
+void _mul(stack_t **head, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);

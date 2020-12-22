99c1930659b2ce55e332786cfc16e89b10ee525c
m33x0 <michael.mbea@gmail.com>
2020-12-22 19:11:22 +0100

    addded prototype

--- monty.h
+++ monty.h
@@ -54,6 +54,7 @@ void _sub(stack_t **head, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _add(stack_t **head, unsigned int line_number);
 void _mod(stack_t **head, unsigned int line_number);
+void _pchar(stack_t **head, unsigned int line_number);
 int parse(char *buffer, stack_t **stack, int number_line);
 int _opcodes(char *opcode, stack_t **stack, int number_line);
 

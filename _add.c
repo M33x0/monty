d6b72226a2b9f8db5f41912932b441e9c2c13d10
m33x0 <michael.mbea@gmail.com>
2020-12-22 17:44:43 +0100

    add two elements of stack

---
+++ _add.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * _add - adds the top two elements of the stack
+ *
+ * @head: head of a stack_t
+ * @line_number: line nbr of the opcode in the read file
+ *
+ * Return: void
+ */
+
+void _add(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	stack_t *previous;
+	int res = 0;
+
+	current = *head;
+	if (current == NULL || current->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		code_exit = -1;
+		return;
+	}
+	previous = current->prev;
+	res = current->n + previous->n;
+	previous->n = res;
+	previous->next = NULL;
+	free(current);
+	*head = previous;
+}

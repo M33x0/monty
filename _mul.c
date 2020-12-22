87df0c162be695a05ed6b964bc808134381e16b4
m33x0 <michael.mbea@gmail.com>
2020-12-22 18:36:52 +0100

    ffs

---
+++ _mul.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * _mul - multiplies the seconde top element of the stack by the top
+ *
+ * @head: head of a stack_t
+ * @line_number: line nbr of the opcode in the read file
+ *
+ * Return: void
+ */
+
+void _mul(stack_t **head, unsigned int line_number)
+{
+	stack_t *current;
+	stack_t *previous;
+	int res = 0;
+
+	current = *head;
+	if (current == NULL || current->prev == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
+		code_exit = -1;
+		return;
+	}
+	previous = current->prev;
+	res = previous->n * current->n;
+	previous->n = res;
+	previous->next = NULL;
+	free(current);
+	*head = previous;
+}
